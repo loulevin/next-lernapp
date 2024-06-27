eb8fc29810ef79440b2295d7645ea69e265bebd8
loulevin <patricia.balbier@gmail.com>
2024-06-27 15:52:02 +0200

    chore: Update navbar layout and styles

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -1,18 +1,22 @@
+'use client'
+
 import { navbarLinks } from "@/constants";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
     <>
-      <section className="w-screen">
+      <section className="w-screen width-responive">
         <nav className="navbar-layout">
-          {navbarLinks.map((link) => {
-            return (
-              <Link href={link.route} key={link.label} className="">
-                <p className="">{link.label}</p>
-              </Link>
-            );
-          })}
+        {navbarLinks.map((link) => (
+            <Link href={link.route} key={link.label} className="nav-link">
+              <p className={`nav-label ${router.pathname === link.route ? 'active' : ''}`}>
+                {link.label}
+              </p>
+            </Link>
+          ))}
         </nav>
       </section>
     </>
