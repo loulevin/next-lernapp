9c0b43544d5929083a0a7bf7119689e066d61eda
loulevin <patricia.balbier@gmail.com>
2024-07-31 04:18:00 +0200

    add table and card in the pages

--- app/(root)/training/page.tsx
+++ app/(root)/training/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { IVocabel } from "@/lib/interfaces";
+import { useState, useEffect } from "react";
+import * as config from "@/lib/config";
+import axios from "axios";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function training() {
+  const [vocabel, setVocabel] = useState<IVocabel[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(`${config.backendUrl()}/vocabels`);
+      const _vocabels = response.data;
+      setVocabel(_vocabels);
+    })();
+  }, []);
+
   return (
     <>
       <h1>Training</h1>
+      {vocabel.map((vocabel, index) => {
+        return (
+          <Card key={index} className="w-[35rem] bg-slate-400">
+            <CardHeader>
+              <CardTitle className="text-center bg-teal-500 p-4">{vocabel.japanese}</CardTitle>
+            </CardHeader>
+          </Card>
+        );
+      })}
     </>
   );
 }
--- app/(root)/vocabulary/page.tsx
+++ app/(root)/vocabulary/page.tsx
@@ -1,28 +1,52 @@
-'use client'
+"use client";
 
 import { IVocabel } from "@/lib/interfaces";
-import { useState, useEffect } from "react"; 
-import * as config from "@/lib/config"
+import { useState, useEffect } from "react";
+import * as config from "@/lib/config";
 import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function vocabeltraining() {
-  const [ vocabel, setVocabel] = useState<IVocabel[]>([]);
+  const [vocabel, setVocabel] = useState<IVocabel[]>([]);
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get(`${config.backendUrl()}/vocabels`)
-      const _vocabels = response.data
-      setVocabel(_vocabels)
-    })()
-  }, [])
+      const response = await axios.get(`${config.backendUrl()}/vocabels`);
+      const _vocabels = response.data;
+      setVocabel(_vocabels);
+    })();
+  }, []);
+
   return (
     <>
       <h1>Vokabeln</h1>
-      {vocabel.map((vocabel, index) => {
-        return (
-          <li key={index}><span className="font-semibold">{vocabel.japanese}</span> - {vocabel.reading}</li>
-        )
-      })}
+    
+
+      <Table>
+        <TableHeader>
+          <TableHead>Japanisch</TableHead>
+          <TableHead>Lesung</TableHead>
+          <TableHead>Kanji</TableHead>
+          <TableHead>Deutsche Übersetzung</TableHead>
+        </TableHeader>
+        <TableBody>
+          {vocabel.map((vocabel, index) => (
+            <TableRow key={index}>
+              <TableCell>{vocabel.japanese}</TableCell>
+              <TableCell>{vocabel.reading}</TableCell>
+              <TableCell>{vocabel.kanji}</TableCell>
+              <TableCell>{vocabel.translate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   );
 }
